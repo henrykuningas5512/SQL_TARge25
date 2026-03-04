720d4382dd788c83ff58e4c90aa05e4d8421b85d
Opilane TTHK <opilane@tthk.ee>
2026-03-04 13:26:28 +0200

    tegin db

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+create database TARge25
+
+--db valimine
+use master
+
+--db kustutamine
+drop database TARge25
+
+--tabeli tegemine
+create table Gender
+(
+Id int not null primary key,
+Gender nvarchar (10) not null
+)
